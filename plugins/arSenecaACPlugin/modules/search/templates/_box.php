d4547d206461288034bb665cd66f27ae4d9afac6
J Foo <grumpel7@hotmail.com>
2016-07-14 17:07:40 -0400

    arArchivesCanada Plugin search was not working, so ...
    saved the AC plugin version of the _box.php file, grabbed the default
    one from apps/qubit/modules/search.templates and copied it into the
    arSenecaACPlugin folder. Removed the <h2> "Search" title tag to move
    search box up.  Still need to fix width of the search page contents

--- plugins/arSenecaACPlugin/modules/search/templates/_box.php
+++ plugins/arSenecaACPlugin/modules/search/templates/_box.php
@@ -1,56 +1,16 @@
-<div id="search-form-wrapper">
+<div id="search-form-wrapper" role="search">
 
-  <form action="<?php echo url_for(array('module' => 'search')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>">
+  <form action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>" autocomplete="off">
 
-    <div class="input-append">
+    <input type="hidden" name="topLod" value="0"/>
 
-      <?php if (isset($repository)): ?>
-        <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Search %1%', array('%1%' => render_title($repository))) ?>"/>
-      <?php else: ?>
-        <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Search') ?>"/>
-      <?php endif; ?>
+    <?php if (isset($repository)): ?>
+      <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Search %1%', array('%1%' => render_title($repository))) ?>"/>
+    <?php else: ?>
+      <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Search') ?>"/>
+    <?php endif; ?>
 
-      <div class="btn-group">
-        <button class="btn dropdown-toggle" data-toggle="dropdown">
-          <span class="caret"></span>
-        </button>
-        <?php $cacheKey = 'search-box-nav-'.$sf_user->getCulture() ?>
-        <?php if (!cache($cacheKey)): ?>
-          <ul class="dropdown-menu pull-right">
-            <?php $icons = array(
-              'browseInformationObjects' => '/images/icons-large/icon-archival.png',
-              'browseActors' => '/images/icons-large/icon-people.png',
-              'browseRepositories' => '/images/icons-large/icon-institutions.png',
-              'browseSubjects' => '/images/icons-large/icon-subjects.png',
-              'browseFunctions' => '/images/icons-large/icon-functions.png',
-              'browsePlaces' => '/images/icons-large/icon-places.png',
-              'browseDigitalObjects' => '/images/icons-large/icon-media.png') ?>
-            <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
-            <?php if ($browseMenu->hasChildren()): ?>
-              <?php foreach ($browseMenu->getChildren() as $item): ?>
-                <li>
-                  <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
-                    <?php if (isset($icons[$item->name])): ?>
-                      <?php echo image_tag($icons[$item->name], array('width' => 42, 'height' => 42, 'alt' => '')) ?>
-                    <?php endif; ?>
-                    <?php echo esc_entities($item->getLabel(array('cultureFallback' => true))) ?>
-                  </a>
-                </li>
-              <?php endforeach; ?>
-            <?php endif; ?>
-            <li class="divider"></li>
-            <li class="advanced-search">
-              <a href="<?php echo url_for(array('module' => 'search', 'action' => 'advanced')) ?>">
-                <i class="icon-zoom-in"></i>
-                <?php echo __('Advanced search') ?>
-              </a>
-            </li>
-          </ul>
-          <?php cache_save($cacheKey) ?>
-        <?php endif; ?>
-      </div>
-
-    </div>
+    <button><span><?php echo __('Search') ?></span></button>
 
     <div id="search-realm" class="search-popover">
 
@@ -70,7 +30,7 @@
         <?php if (isset($repository)): ?>
           <div>
             <label>
-              <input name="repos" checked="checked" type="radio" value="<?php echo $repository->id ?>"/>
+              <input name="repos" checked="checked" type="radio" value="<?php echo $repository->id ?>" data-placeholder="<?php echo __('Search %1%', array('%1%' => render_title($repository))) ?>"/>
               <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($repository))) ?>
             </label>
           </div>
@@ -79,7 +39,7 @@
         <?php if (isset($altRepository)): ?>
           <div>
             <label>
-              <input name="repos" type="radio" value="<?php echo $altRepository->id ?>"/>
+              <input name="repos" type="radio" value="<?php echo $altRepository->id ?>" data-placeholder="<?php echo __('Search %1%', array('%1%' => render_title($altRepository))) ?>"/>
               <?php echo __('Search <span>%1%</span>', array('%1%' => render_title($altRepository))) ?>
             </label>
           </div>
@@ -88,7 +48,7 @@
       <?php endif; ?>
 
       <div class="search-realm-advanced">
-        <a href="<?php echo url_for(array('module' => 'search', 'action' => 'advanced')) ?>">
+        <a href="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse', 'showAdvanced' => true, 'topLod' => false)) ?>">
           <?php echo __('Advanced search') ?>&nbsp;&raquo;
         </a>
       </div>
