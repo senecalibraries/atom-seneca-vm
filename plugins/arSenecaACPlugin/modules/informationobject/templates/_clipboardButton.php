e1d882572c7d52ff2dedfd499102f02a83731947
vagrant <vagrant@vagrant.vm>
2016-10-18 15:59:25 +0000

    deleted informationobjects module

--- plugins/arSenecaACPlugin/modules/informationobject/templates/_clipboardButton.php
+++
@@ -1,8 +0,0 @@
-<button class="<?php echo $class ?>"
-  data-clipboard-url="<?php echo $url ?>"
-  data-clipboard-slug="<?php echo $slug ?>"
-  <?php echo $tooltip ? 'data-toggle="tooltip"' : '' ?>
-  data-title="<?php echo $title ?>"
-  data-alt-title="<?php echo $altTitle ?>">
-  <?php echo $title ?>
-</button>
